394e4a90de862f71291ad2212c3412b3624d3b13
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-19 17:09:12 -0300

    Configurando testes de integração na API HTTP

--- src/test/java/com/example/tdd/PeopleCatalogApplicationTests.java
+++ src/test/java/com/example/tdd/PeopleCatalogApplicationTests.java
@@ -2,9 +2,19 @@ package com.example.tdd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
-@SpringBootTest
+@Sql(value = "/load-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestPropertySource("classpath:application-test.properties")
 class PeopleCatalogApplicationTests {
+
+	@LocalServerPort
+	protected int porta;
 
 	@Test
 	void contextLoads() {
