cc91f7e3a041ca5d593c2830a7175a253ca694f0
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-19 13:45:06 -0300

    Criando primeiro teste de Integração com o banco de dados

--- src/main/java/com/example/tdd/repository/PessoaRepository.java
+++ src/main/java/com/example/tdd/repository/PessoaRepository.java
@@ -3,6 +3,7 @@ package com.example.tdd.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.tdd.modelo.Pessoa;
 
@@ -10,6 +11,8 @@ public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
 	Optional<Pessoa> findByCpf(String cpf);
 
+	// TODO Query temporaria, será removida futuramente
+	@Query("SELECT bean FROM Pessoa bean WHERE 1=1")
 	Optional<Pessoa> findByTelefoneAndTelefoneNumero(String ddd, String numero);
 
 }
---
+++ src/test/java/com/example/tdd/repository/PessoaRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.example.tdd.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.tdd.modelo.Pessoa;
+
+@Sql(value = "/load-database.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clean-database.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@TestPropertySource("classpath:application-test.properties")
+public class PessoaRepositoryTest {
+
+	@Autowired
+	private PessoaRepository sut;
+
+	@Test
+	public void deveProcurarPessoaPeloCPF() throws Exception {
+		Optional<Pessoa> optional = sut.findByCpf("38767897100");
+
+		assertThat(optional.isPresent()).isTrue();
+
+		Pessoa pessoa = optional.get();
+		assertThat(pessoa.getCodigo()).isEqualTo(3L);
+		assertThat(pessoa.getNome()).isEqualTo("Cauê");
+		assertThat(pessoa.getCpf()).isEqualTo("38767897100");
+	}
+	
+	@Test
+	public void naoDeveEncontrarPessoaDeCPFInexistente() throws Exception {
+		Optional<Pessoa> optional = sut.findByCpf("85165164681");
+
+		assertThat(optional.isPresent()).isFalse();
+	}
+	
+}
