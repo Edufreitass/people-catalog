4981a6ea2828b211ee6c9dabdcdc83350b56653d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-20 10:28:04 -0300

    Salvando nova pessoa

--- src/main/java/com/example/tdd/resource/PessoaResource.java
+++ src/main/java/com/example/tdd/resource/PessoaResource.java
@@ -1,17 +1,26 @@
 package com.example.tdd.resource;
 
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.tdd.modelo.Pessoa;
 import com.example.tdd.modelo.Telefone;
 import com.example.tdd.servico.PessoaService;
 import com.example.tdd.servico.exception.TelefoneNaoEncontradoException;
+import com.example.tdd.servico.exception.UnicidadeCpfException;
+import com.example.tdd.servico.exception.UnicidadeTelefoneException;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -32,4 +41,16 @@ public class PessoaResource {
 		return new ResponseEntity<>(pessoa, HttpStatus.OK);
 	}
 
+	@PostMapping
+	public ResponseEntity<Pessoa> salvarNova(@RequestBody Pessoa pessoa, HttpServletResponse response) throws UnicidadeCpfException, UnicidadeTelefoneException {
+		final Pessoa pessoaSalva = service.salvar(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{ddd}/{numero}")
+				.buildAndExpand(pessoa.getTelefones().get(0).getDdd(), pessoa.getTelefones().get(0).getNumero()).toUri();
+		
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return new ResponseEntity<>(pessoaSalva, HttpStatus.CREATED);
+	}
+	
 }
--- src/test/java/com/example/tdd/resources/PessoaResourceTest.java
+++ src/test/java/com/example/tdd/resources/PessoaResourceTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import com.example.tdd.PeopleCatalogApplicationTests;
+import com.example.tdd.modelo.Pessoa;
+import com.example.tdd.modelo.Telefone;
+
+import io.restassured.http.ContentType;
 
 public class PessoaResourceTest extends PeopleCatalogApplicationTests {
 
@@ -34,6 +38,36 @@ public class PessoaResourceTest extends PeopleCatalogApplicationTests {
 				.log().body().and()
 				.statusCode(HttpStatus.NOT_FOUND.value())
 				.body("message", equalTo("Não existe pessoa com o telefone (99)987654321"));
+	}
+	
+	@Test
+	void deveSalvarNovaPessoaNoSistema() throws Exception {
+		final Pessoa pessoa = new Pessoa();
+		pessoa.setNome("Fernanda");
+		pessoa.setCpf("27161503469");
+		
+		final Telefone telefone = new Telefone();
+		telefone.setDdd("67");
+		telefone.setNumero("37132848");
+		
+		pessoa.adicionaTelefone(telefone);
+		
+		given()
+				.request()
+				.header("Accept", ContentType.ANY)
+				.header("Content-type", ContentType.JSON)
+				.body(pessoa)
+		.when()
+		.post("/pessoas")
+		.then()
+				.log().headers().and()
+				.log().body().and()
+				.statusCode(HttpStatus.CREATED.value())
+				.header("Location", equalTo("http://localhost:" + porta + "/pessoas/67/37132848"))
+				.body("codigo", equalTo(6),
+						"nome", equalTo("Fernanda"),
+						"cpf", equalTo("27161503469"));
+		
 	}
 	
 }
